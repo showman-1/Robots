d825d74b260717ef6cfb564fdf54c882fe7ebb43
showman-1 <vladimir24022006@gmail.com>
2026-03-06 13:50:08 +0500

    add wind "Menu" and "exit"

--- robots/src/gui/MainApplicationFrame.java
+++ robots/src/gui/MainApplicationFrame.java
@@ -10,6 +10,7 @@ import javax.swing.JInternalFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -18,26 +19,26 @@ import log.Logger;
 
 /**
  * Что требуется сделать:
- * 1. Метод создания меню перегружен функционалом и трудно читается. 
+ * 1. Метод создания меню перегружен функционалом и трудно читается.
  * Следует разделить его на серию более простых методов (или вообще выделить отдельный класс).
  *
  */
 public class MainApplicationFrame extends JFrame
 {
     private final JDesktopPane desktopPane = new JDesktopPane();
-    
+
     public MainApplicationFrame() {
         //Make the big window be indented 50 pixels from each edge
         //of the screen.
-        int inset = 50;        
+        int inset = 50;
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setBounds(inset, inset,
-            screenSize.width  - inset*2,
-            screenSize.height - inset*2);
+                screenSize.width  - inset*2,
+                screenSize.height - inset*2);
 
         setContentPane(desktopPane);
-        
-        
+
+
         LogWindow logWindow = createLogWindow();
         addWindow(logWindow);
 
@@ -46,9 +47,9 @@ public class MainApplicationFrame extends JFrame
         addWindow(gameWindow);
 
         setJMenuBar(generateMenuBar());
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
     }
-    
+
     protected LogWindow createLogWindow()
     {
         LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
@@ -59,21 +60,21 @@ public class MainApplicationFrame extends JFrame
         Logger.debug("Протокол работает");
         return logWindow;
     }
-    
+
     protected void addWindow(JInternalFrame frame)
     {
         desktopPane.add(frame);
         frame.setVisible(true);
     }
-    
+
 //    protected JMenuBar createMenuBar() {
 //        JMenuBar menuBar = new JMenuBar();
-// 
+//
 //        //Set up the lone menu.
 //        JMenu menu = new JMenu("Document");
 //        menu.setMnemonic(KeyEvent.VK_D);
 //        menuBar.add(menu);
-// 
+//
 //        //Set up the first menu item.
 //        JMenuItem menuItem = new JMenuItem("New");
 //        menuItem.setMnemonic(KeyEvent.VK_N);
@@ -82,28 +83,42 @@ public class MainApplicationFrame extends JFrame
 //        menuItem.setActionCommand("new");
 ////        menuItem.addActionListener(this);
 //        menu.add(menuItem);
-// 
+//
 //        //Set up the second menu item.
 //        menuItem = new JMenuItem("Quit");
 //        menuItem.setMnemonic(KeyEvent.VK_Q);
 //        menuItem.setAccelerator(KeyStroke.getKeyStroke(
 //                KeyEvent.VK_Q, ActionEvent.ALT_MASK));
 //        menuItem.setActionCommand("quit");
-////        menuItem.addActionListener(this);
+    ////        menuItem.addActionListener(this);
 //        menu.add(menuItem);
-// 
+//
 //        return menuBar;
 //    }
-    
+
     private JMenuBar generateMenuBar()
     {
         JMenuBar menuBar = new JMenuBar();
-        
+        // НОВОЕ МЕНЮ "Файл"
+        JMenu fileMenu = new JMenu("Файл");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+
+        // Пункт "Выход"
+        {
+            JMenuItem exitMenuItem = new JMenuItem("Выход", KeyEvent.VK_X);
+            exitMenuItem.addActionListener((event) -> {
+                exitApplication();
+            });
+            fileMenu.add(exitMenuItem);
+        }
+
+        menuBar.add(fileMenu);
+
         JMenu lookAndFeelMenu = new JMenu("Режим отображения");
         lookAndFeelMenu.setMnemonic(KeyEvent.VK_V);
         lookAndFeelMenu.getAccessibleContext().setAccessibleDescription(
                 "Управление режимом отображения приложения");
-        
+
         {
             JMenuItem systemLookAndFeel = new JMenuItem("Системная схема", KeyEvent.VK_S);
             systemLookAndFeel.addActionListener((event) -> {
@@ -126,7 +141,7 @@ public class MainApplicationFrame extends JFrame
         testMenu.setMnemonic(KeyEvent.VK_T);
         testMenu.getAccessibleContext().setAccessibleDescription(
                 "Тестовые команды");
-        
+
         {
             JMenuItem addLogMessageItem = new JMenuItem("Сообщение в лог", KeyEvent.VK_S);
             addLogMessageItem.addActionListener((event) -> {
@@ -139,7 +154,7 @@ public class MainApplicationFrame extends JFrame
         menuBar.add(testMenu);
         return menuBar;
     }
-    
+
     private void setLookAndFeel(String className)
     {
         try
@@ -148,9 +163,35 @@ public class MainApplicationFrame extends JFrame
             SwingUtilities.updateComponentTreeUI(this);
         }
         catch (ClassNotFoundException | InstantiationException
-            | IllegalAccessException | UnsupportedLookAndFeelException e)
+               | IllegalAccessException | UnsupportedLookAndFeelException e)
         {
             // just ignore
         }
+    }
+    /**
+     * НОВЫЙ МЕТОД: Обработка выхода из приложения с подтверждением
+     */
+    private void exitApplication()
+    {
+        // Настраиваем кнопки диалога на русском языке
+        Object[] options = {"Да", "Нет", "Отмена"};
+
+        int result = JOptionPane.showOptionDialog(
+                this,                                   // родительский компонент
+                "Вы действительно хотите выйти из приложения?", // сообщение
+                "Подтверждение выхода",                 // заголовок
+                JOptionPane.YES_NO_CANCEL_OPTION,       // тип опций
+                JOptionPane.QUESTION_MESSAGE,           // тип сообщения
+                null,                                    // иконка (по умолчанию)
+                options,                                 // массив кнопок
+                options[1]);                             // кнопка по умолчанию ("Нет")
+
+        // Обрабатываем результат
+        if (result == JOptionPane.YES_OPTION) {
+            // Пользователь подтвердил выход
+            Logger.debug("Приложение завершает работу");
+            System.exit(0);
+        }
+        // В остальных случаях (Нет, Отмена, закрытие диалога) просто возвращаемся
     }
 }
