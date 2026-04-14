ca33453247b81ddc893a79178d18e2b016f17902
showman-1 <vladimir24022006@gmail.com>
2026-04-14 14:34:33 +0500

    moving reading and writing of the config to a separate class

--- robots/src/gui/WindowStateManager.java
+++ robots/src/gui/WindowStateManager.java
@@ -1,98 +1,33 @@
 package gui;
 
-import java.awt.Point;
-import java.io.*;
 import java.util.Properties;
 import javax.swing.JInternalFrame;
 
 public class WindowStateManager {
 
-    private static final String CONFIG_FILE = System.getProperty("user.home") +
-            File.separator +
-            ".robot_game_config.properties";
-
     private Properties properties;
+    private WindowStateSaver saver;
+    private WindowStateLoader loader;
 
     public WindowStateManager() {
-        properties = new Properties();
+        this.properties = new Properties();
+        this.saver = new WindowStateSaver();
+        this.loader = new WindowStateLoader();
     }
 
-    public void saveWindowState(JInternalFrame frame, String windowKey) {
-        try {
-            Point location = frame.getLocation();
-            properties.setProperty(windowKey + ".x", String.valueOf(location.x));
-            properties.setProperty(windowKey + ".y", String.valueOf(location.y));
-
-            properties.setProperty(windowKey + ".width", String.valueOf(frame.getWidth()));
-            properties.setProperty(windowKey + ".height", String.valueOf(frame.getHeight()));
-
-            if (frame.isIcon()) {
-                properties.setProperty(windowKey + ".state", "ICONIFIED");
-            } else if (frame.isMaximum()) {
-                properties.setProperty(windowKey + ".state", "MAXIMIZED");
-            } else {
-                properties.setProperty(windowKey + ".state", "NORMAL");
-            }
-
-        } catch (Exception e) {
-            System.err.println("Ошибка сохранения состояния окна " + windowKey + ": " + e.getMessage());
-        }
+    public void loadFromFile() {
+        this.properties = loader.loadFromFile();
     }
 
     public void saveToFile() {
-        try (FileOutputStream out = new FileOutputStream(CONFIG_FILE)) {
-            properties.store(out, "Robot Game Configuration");
-        } catch (IOException e) {
-            System.err.println("Ошибка сохранения конфигурации: " + e.getMessage());
-        }
+        saver.saveToFile(properties);
     }
 
-    public boolean loadFromFile() {
-        File configFile = new File(CONFIG_FILE);
-        if (!configFile.exists()) {
-            return false;
-        }
-
-        try (FileInputStream in = new FileInputStream(configFile)) {
-            properties.load(in);
-            return true;
-        } catch (IOException e) {
-            System.err.println("Ошибка загрузки конфигурации: " + e.getMessage());
-            return false;
-        }
+    public void saveWindowState(JInternalFrame frame, String windowKey) {
+        saver.saveWindowState(properties, frame, windowKey);
     }
 
     public void applyWindowState(JInternalFrame frame, String windowKey) {
-        try {
-            String xStr = properties.getProperty(windowKey + ".x");
-            String yStr = properties.getProperty(windowKey + ".y");
-            if (xStr != null && yStr != null) {
-                frame.setLocation(Integer.parseInt(xStr), Integer.parseInt(yStr));
-            }
-
-            String widthStr = properties.getProperty(windowKey + ".width");
-            String heightStr = properties.getProperty(windowKey + ".height");
-            if (widthStr != null && heightStr != null) {
-                frame.setSize(Integer.parseInt(widthStr), Integer.parseInt(heightStr));
-            }
-
-            String state = properties.getProperty(windowKey + ".state");
-            if (state != null) {
-                switch (state) {
-                    case "ICONIFIED":
-                        frame.setIcon(true);
-                        break;
-                    case "MAXIMIZED":
-                        try {
-                            frame.setMaximum(true);
-                        } catch (Exception e) {
-                        }
-                        break;
-                }
-            }
-
-        } catch (Exception e) {
-            System.err.println("Ошибка восстановления окна " + windowKey + ": " + e.getMessage());
-        }
+        loader.applyWindowState(properties, frame, windowKey);
     }
 }
