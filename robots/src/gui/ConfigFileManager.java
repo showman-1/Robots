71143730440cdf63bce776e47d4f3302da2c5921
showman-1 <vladimir24022006@gmail.com>
2026-04-21 19:40:13 +0500

    change logic

--- robots/src/gui/ConfigFileManager.java
+++ robots/src/gui/ConfigFileManager.java
@@ -6,20 +6,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-/**
- * Отвечает ТОЛЬКО за чтение и запись файла конфигурации.
- * Ничего не знает об окнах, только работа с Properties и файлом.
- */
+
 public class ConfigFileManager {
 
     private static final String CONFIG_FILE = System.getProperty("user.home") +
             File.separator +
             ".robot_game_config.properties";
 
-    /**
-     * Загружает настройки из файла
-     * @return Properties с загруженными настройками, или пустые Properties если файла нет
-     */
+
     public Properties loadFromFile() {
         Properties properties = new Properties();
         File configFile = new File(CONFIG_FILE);
@@ -37,10 +31,7 @@ public class ConfigFileManager {
         return properties;
     }
 
-    /**
-     * Сохраняет настройки в файл
-     * @param properties настройки для сохранения
-     */
+
     public void saveToFile(Properties properties) {
         try (FileOutputStream out = new FileOutputStream(CONFIG_FILE)) {
             properties.store(out, "Robot Game Configuration");
